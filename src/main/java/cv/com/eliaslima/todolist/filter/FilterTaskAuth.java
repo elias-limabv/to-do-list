0df997362093fddacd1b54c7387caf68711863c3
Elias Lima <eliasmoraislima@gmail.com>
2023-10-14 00:10:28 -0100

    class FilterTaskAuth using Filter and doFilter

---
+++ src/main/java/cv/com/eliaslima/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,62 @@
+package cv.com.eliaslima.todolist.filter;
+
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import cv.com.eliaslima.todolist.user.IUserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException{
+
+        var servletPath = request.getServletPath();
+
+        if (servletPath.startsWith("/tasks/")) {
+            // Pegar a autenticação (usuario e senha)
+            var authority = request.getHeader("Authorization");
+
+            var authEncoded = authority.substring("Basic".length()).trim();
+
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+            var authString = new String(authDecode);
+            System.out.println("Authorization");
+            String[] credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
+
+            // validar usuario
+            var user = this.userRepository.findByUsername(username);
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                // validar senha
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+                // Segue viagem
+            }
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
